57cce70e32c7b1e97fecb4487dfe118cd4bbc9e4
Frosty <113263038+zqeus@users.noreply.github.com>
2023-03-18 11:49:57 +0000

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-var wbhk = 'https://discord.com/api/webhooks/1086616367177932881/kKGpw86ogScKNDf2TObjpLzXb6FN1yffrTIfcoaghOpGq6Dlddc0h-dJQu0dkoPQJyND'
-var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
