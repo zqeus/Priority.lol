5340c84650787902d9f5ea3360d64d494780235f
Frosty <113263038+zqeus@users.noreply.github.com>
2023-04-03 16:13:17 +0100

    Delete darkmodestuff.js

--- darkmodestuff.js
+++
@@ -1,5 +0,0 @@
-function toggleDarkLight() {
-    let body = document.getElementById("body");
-    let currentClass = body.className;
-    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-  }
